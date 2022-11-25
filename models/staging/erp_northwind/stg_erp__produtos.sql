870635825831f2ee330b8de73338dcf5b72dc66d
academy.turma.202211@gmail.com <academy.turma.202211@gmail.com>
2022-11-25 14:39:56 +0000

    feito cast correto e ajustado nome de colunas no stg_produtos

--- models/staging/erp_northwind/stg_erp__produtos.sql
+++ models/staging/erp_northwind/stg_erp__produtos.sql
@@ -6,10 +6,10 @@ with
             , cast(category_id as int) as id_categoria	
             , cast(product_name as string)as nome_produto			
             , cast(quantity_per_unit as string)	as quantidade_por_unidade		
-            , unit_price as preco_por_unidade
-            , cast(units_in_stock as int) as unidades_no_estoque		
+            , cast(unit_price as numeric) as preco_por_unidade
+            , cast(units_in_stock as int) as unidades_em_estoque		
             , cast(units_on_order as int) as unidades_por_ordem			
-            , cast(reorder_level as int) as	level_de_pedido		
+            , cast(reorder_level as int) as	nivel_reabastecimento		
             , case
                 when discontinued = 1 then true
                 else false
