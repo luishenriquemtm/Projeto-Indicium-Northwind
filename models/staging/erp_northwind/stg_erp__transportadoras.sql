f4dc1b3ee2a248bb0d5956150e51ecb63676217b
Luis Henrique Barbosa da Silva <luishenriquemtm@gmail.com>
2022-12-01 01:14:39 +0000

    Projeto do aulão replicado, editado e organizado.

--- models/staging/erp_northwind/stg_erp__transportadoras.sql
+++ models/staging/erp_northwind/stg_erp__transportadoras.sql
@@ -1,20 +1,10 @@
 with
     source_transportadoras as (
-<<<<<<< HEAD
         select 
             cast (shipper_id as int) as id_transportadora					
-            ,cast (company_name as string) as nome_do_transportadora					
-            ,cast (phone as string) as telefone			
+            ,cast (company_name as string) as nome_transportadora					
+            ,cast (phone as string) as telefone_transportadora			
         from {{source('erp','shippers')}}
     )
-=======
-        select
-            cast(shipper_id	as int) as id_transportadora
-            , cast(company_name as string) as nome_transportadora
-            , cast(phone as string) as telefone_transportadora
-        from {{ source('erp', 'shippers') }}
-    )
-
->>>>>>> e33304deede8814703167c7f809bedf8a9d6d2e9
 select *
 from source_transportadoras
